9976d942b44d1d4289bc19c58a694581057d24c7
Galih Rizki Setiadi <galihrizkisetiadi18@gmail.com>
2025-08-29 19:34:53 +0700

    fix: establish routing in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,12 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('hospital') }}">{{ __('Rumah Sakit') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('patient') }}">{{ __('Pasien') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\PatientController;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HospitalController::class, 'index'])->name('hospital');
+Route::get('/', [App\Http\Controllers\HospitalController::class, 'index'])->name('index');
 
 Route::middleware([Authenticate::class])->group(function () {
-    Route::resource('hospital', HospitalController::class);
+    Route::resource('hospital', HospitalController::class)->name('index', 'hospital');
+    Route::resource('patient', PatientController::class)->name('index', 'patient');
 });
