559ac345ee2679fcca166852475be44496c26102
Galih Rizki Setiadi <galihrizkisetiadi18@gmail.com>
2025-08-29 19:21:41 +0700

    fix: setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HospitalController;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HospitalController::class, 'index'])->name('hospital');
+
+Route::middleware([Authenticate::class])->group(function () {
+    Route::resource('hospital', HospitalController::class);
+});
